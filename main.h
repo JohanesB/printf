8fd34130f4501aacdb70dc8032c7ba30978cd409
Yohanes <yohanesbacha@gmail.com>
2022-07-11 14:06:32 -0700

    produces output

--- main.h
+++ main.h
@@ -1,5 +1,18 @@
 #ifndef _ALX_
 #define _ALx_
+#include <stdarg.h>
+
+
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
 
 int _printf(const char *format, ...);
 
