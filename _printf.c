4e4d82fd4bacabe53aa422662a3635b784ab7b6f
Samrawit Girma <abababmgmgmg@gmail.com>
2022-07-12 04:32:43 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -10,15 +10,19 @@
  */
 int _printf(const char *format, ...)
 {
+	buffer_t *output;
 	va_list args;
 	int length = 0;
 
 	if (format == NULL)
 		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
 	va_start(args, format);
 
-	length = _print_format(format, args);
-	va_end(args);
+	length = _print_format(format, args, output);
+
 	return (length);
 }
