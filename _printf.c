dc8074093e9051b8a76e5827f30e2a1397563952
Samrawit Girma <abababmgmgmg@gmail.com>
2022-07-12 10:31:45 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -10,19 +8,39 @@
  */
 int _printf(const char *format, ...)
 {
-	buffer_t *output;
-	va_list args;
-	int length = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (format == NULL)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	output = init_buffer();
-	if (output == NULL)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 
-	va_start(args, format);
-
-	length = _print_format(format, args, output);
-
-	return (length);
 }
